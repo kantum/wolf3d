4fb5e53454ffc1da77a852608526fa5cf01d6425
Vimman <vimman@users.noreply.github.com>
2017-11-30 00:48:36 +0100

    clean base

--- includes/wolf.h
+++ includes/wolf.h
@@ -1,14 +1,14 @@
-#include <unistd.h>
-#include <math.h>
-#include <stdio.h>
+# include <unistd.h>
+# include <math.h>
+# include <stdio.h>
 # include "mlx.h"
 # include "libft.h"
 
-#define mapWidth 24
-#define mapHeight 24
+# define mapWidth 24
+# define mapHeight 24
 
-#define WIDTH 1280
-#define HEIGHT 800
+# define WIDTH 1280
+# define HEIGHT 800
 
 # define KEYPRESS			2
 # define KEYPRESSMASK		(1L<<0)
@@ -19,16 +19,22 @@
 # define DESTROYNOTIFY		17
 # define STRUCTURENOTIFYMASK	(1L<<17)
 
+typedef struct			s_point
+{
+	int		x;
+	int		y;
+	int		color;
+}						t_point;
+
 typedef struct			s_env
 {
 	void		*mlx;
 	void		*win;
-	t_point		origin;
 	int			color;
 	void		*img;
 	int			*data;
 	int			size;
 	int			bpp;
 	int			endian;
-	t_fractal	f;
+	t_point		p;
 }						t_env;
---
+++ srcs/main.c
@@ -0,0 +1,91 @@
+#include "wolf.h"
+
+void	put_pixel(int x, int y, int color, t_env *e)
+{
+	if (y < HEIGHT && x < WIDTH &&
+			y >= 0 && x >= 0)
+		e->data[y * WIDTH + x] = color;
+}
+
+void	init_point(t_point *p)
+{
+	p->x = -1;
+	p->y = -1;
+	p->color = 0xFFFFFF;
+}
+
+int		init(t_env *e)
+{
+	char	*title;
+
+	title = "Wolf 3D";
+	if (!(e->mlx = mlx_init()))
+		return (-1);
+	e->win = mlx_new_window(e->mlx, WIDTH, HEIGHT, title);
+	e->img = mlx_new_image(e->mlx, WIDTH, HEIGHT);
+	e->data = (int *)mlx_get_data_addr(e->img, &e->bpp, &e->size, &e->endian);
+	return (0);
+}
+
+void	draw(t_env *e)
+{
+	int		x;
+	int		y;
+
+	x = -1;
+	y = -1;
+	mlx_clear_window(e->mlx, e->win);
+	while (++x < WIDTH)
+	{
+		while (++y < HEIGHT)
+		{
+			put_pixel(x, y, 0xffffff, e);
+		}
+		y = -1;
+	}
+	mlx_put_image_to_window(e->data, e->win, e->img, 0, 0);
+}
+
+int		key_hook(int keycode, t_env *e)
+{
+	(void)keycode;
+	(void)e;
+	return (0);
+}
+
+int		mouse_hook(int button, int x, int y, t_env *e)
+{
+	(void)e;
+	(void)button;
+	(void)x;
+	(void)y;
+	return (0);
+}
+
+int		expose_hook(t_env *e)
+{
+	(void)e;
+	return(0);
+}
+
+
+int		quit(t_env *e)
+{
+	mlx_destroy_window(e->mlx, e->win);
+	exit(0);
+}
+
+int		main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	t_env	e;
+	
+	init(&e);
+	draw(&e);
+	mlx_hook(e.win, 17, (1L << 17), quit, &e);
+	mlx_key_hook(e.win, key_hook, &e);
+	mlx_mouse_hook(e.win, mouse_hook, &e);
+	mlx_expose_hook(e.win, expose_hook, &e);
+	mlx_loop(e.mlx);
+}
